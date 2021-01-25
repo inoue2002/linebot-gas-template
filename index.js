96e9c78fe260752eba8de97534aec306f6e942d4
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2021-01-25 21:27:01 +0900

    template

---
+++ index.js
@@ -0,0 +1,64 @@
+// @ts-nocheck
+// LINE developersのメッセージ送受信設定に記載のアクセストークン
+var ACCESS_TOKEN =
+  "<アクセストークンを記入する>";
+async function doPost(e) {
+  for (let i = 0; i < JSON.parse(e.postData.contents).events.length; i++) {
+    const event = JSON.parse(e.postData.contents).events[i];
+    const message = await eventHandle(event);
+    //応答するメッセージがあった場合
+    if (message !== undefined) {
+      const replyToken = event.replyToken;
+      const replyUrl = "https://api.line.me/v2/bot/message/reply";
+      UrlFetchApp.fetch(replyUrl, {
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+          Authorization: "Bearer " + ACCESS_TOKEN,
+        },
+        method: "post",
+        payload: JSON.stringify({
+          replyToken: replyToken,
+          messages: [message],
+        }),
+      });
+    }
+  }
+  return ContentService.createTextOutput(
+    JSON.stringify({ content: "post ok" })
+  ).setMimeType(ContentService.MimeType.JSON);
+}
+
+async function eventHandle(event) {
+  let message;
+  switch (event.type) {
+    case "message":
+      message = await messagefunc(event);
+      break;
+    case "postback":
+      message = await postbackFunc(event);
+      break;
+    case "follow":
+      message = await followFunc(event);
+      break;
+    case "unfollow":
+      message = unfolowFunc(event);
+      break;
+  }
+  return message;
+}
+//メッセージイベントの処理
+async function messagefunc(event) {
+  return { type: "text", text: event.message.text };
+}
+//ポストバックイベントの処理
+async function postbackFunc(event) {
+  return { type: "text", text: event.postback.data };
+}
+//友達登録時の処理
+async function followFunc(event) {
+  return { type: "text", text: "友達登録ありがとうございます!!" };
+}
+//友達解除後の処理
+async function unfollowFunc() {
+  return undefined;
+}
